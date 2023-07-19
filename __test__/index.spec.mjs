33426fd667fed69b28bb644b73c0e9518702b035
skdhg <46562212+skdhg@users.noreply.github.com>
2023-07-19 20:31:43 +0545

    0.0.1

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -4,13 +4,15 @@ import { readFile } from 'fs/promises';
 
 test('should probe mp3 buffer', async (t) => {
   const chunk = await readFile('./__test__/data/head.mp3');
+  const probeResult = probe(chunk);
 
-  t.deepEqual(probe(chunk), {
+  t.deepEqual(probeResult, {
     channels: 2,
     sampleRate: 44100,
     framesPerBlock: 0,
     codec: 4099,
     nFrames: 796032,
-    duration: 18
+    duration: 18,
+    metadata: probeResult.metadata.slice()
   });
 })
